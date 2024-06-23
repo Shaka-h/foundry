254f6cce5b34e2753c08a82db02b69999f300454
Shaka-h <miriam.steven.shaka@gmail.com>
2024-06-23 14:57:56 +0300

    ###

--- script/interactions.s copy.sol
+++ script/interactions.s copy.sol
@@ -3,36 +3,40 @@ pragma solidity ^0.8.0;
 
 import "forge-std/Script.sol";
 import {ProfileFactory} from "../src/ProfileFactory.sol";
+import {DevOpsTools} from "../lib/foundry-devops/src/DevOpsTools.sol";
 
-contract DeployProfileScript is Script {
-    function run() external {
-        // Address of the deployed ProfileFactory contract
-        address factoryContractAddress = 0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512; // Update to your actual contract address
-
-        // Initialize the contract interface
-        ProfileFactory profileFactory = ProfileFactory(factoryContractAddress);
-
-        // Parameters for the profile contract deployment
-        address tweetContractAddress = 0x5FbDB2315678afecb367f032d93F642f64180aa3; // Replace with an actual tweet contract address
-        string memory username = "shaka";
-        string memory profileUrl = "Miriam Shaka";
-
-        // Start broadcasting transactions
+contract DeployProfileFactory is Script {
+    function run() external returns (ProfileFactory) {
         vm.startBroadcast();
 
-        // Call the function to deploy the profile contract
-        address newProfileAddress = profileFactory.deployNFTProfileContract(tweetContractAddress, username, profileUrl);
+        ProfileFactory profileContract = new ProfileFactory();
 
-        // Log the new profile contract address
+        address tweetContractAddress = 0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512; // Replace with an actual tweet contract address
+        string memory username = "shaka";
+        string memory profileUrl = "QmeuQj2qCPTXHp2xHSgaB2hTNajReKm4LUqjWroBBFbp7b";
+
+        console.log("New ProfileFactory Contract Address:", profileContract.getprofileByAddressOwner().ProfileContract);
+
+        address newProfileAddress = profileContract.deployNFTProfileContract(tweetContractAddress, username, profileUrl);
+
         console.log("New Profile Contract Address:", newProfileAddress);
 
-        // Stop broadcasting transactions
+        ProfileFactory.MyNFTProfile memory profile = profileContract.getprofileByAddressContract(newProfileAddress);
+
+        console.log("Profile owner:", profile.owner);
+        console.log("Profile ProfileContract:", profile.ProfileContract);
+        console.log("Profile username:", profile.username);
+        console.log("Profile profileUrl:", profile.profileUrl);
+        console.log("Profile time:", profile.time);
+
         vm.stopBroadcast();
+
+        return profileContract;
     }
 }
 
 contract profileByAddress is Script {
-    function run() external {
+       function run() external {
         // Address of the deployed ProfileFactory contract
         address profileContract = 0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512;
 
@@ -46,7 +50,8 @@ contract profileByAddress is Script {
         vm.startBroadcast();
 
         // Call the function to get the profile by contract address
-        ProfileFactory.MyNFTProfile memory profile = profileFactory.getprofileByAddressContract(profileContractAddress);
+        ProfileFactory.MyNFTProfile memory profile = profileFactory
+            .getprofileByAddressContract(profileContractAddress);
 
         // Log the profile details
         console.log("Profile owner:", profile.owner);
